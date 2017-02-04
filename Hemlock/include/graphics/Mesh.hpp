e5760b1f7ab84de62f63064ca575c8db6a2f2ae0
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-02-04 22:37:56 +0000

    Properly support vertex position, colour and UV data.

--- Hemlock/include/graphics/Mesh.hpp
+++ Hemlock/include/graphics/Mesh.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <type_traits>
 #include <gl\glew.h>
 #include "../types.h"
 
@@ -11,20 +12,25 @@ namespace hemlock {
             STREAM  = GL_STREAM_DRAW
         };
 
-        struct VertexData {
-            VertexData() {};
-            VertexData(f32* positions, f32* colours, ui32* indices, ui32 positionCount, ui32 indexCount) :
-                positions(positions), colours(colours), indices(indices), positionCount(positionCount), indexCount(indexCount) {};
-            f32* positions;
-            f32* colours;
-            ui32* indices;
-            ui32 positionCount;
-            ui32 indexCount;
+        template<typename FXX, typename = std::enable_if_t<std::is_floating_point<FXX>::value>>
+        struct Vertex3D {
+            FXX x, y, z, r, g, b, a, u, v;
+        };
+
+        template<typename FXX, typename = std::enable_if_t<std::is_floating_point<FXX>::value>>
+        struct MeshData3D {
+            Vertex3D<FXX>* vertices;
+            ui32           vertexCount;
+            ui32*          indices;
+            ui32           indexCount;
         };
 
         // TODO(Matthew): Figure out if certain bits (looking at glEnableVertexAttribArray) can't be so-generalised.
         // TODO(Matthew): Make class so we can properly dispose of VBO and EBO.
-        inline GLuint createVAO(VertexData vertexData, VertexDataVolatility volatility = VertexDataVolatility::STATIC) {
+        template<typename FXX, typename = std::enable_if_t<std::is_floating_point<FXX>::value>>
+        inline GLuint createVAO(MeshData3D<FXX> vertexData, VertexDataVolatility volatility = VertexDataVolatility::STATIC) {
+            if (!vertexData.vertices || !vertexData.indices) return 0;
+
             GLuint vao;
             glGenVertexArrays(1, &vao);
             glBindVertexArray(vao);
@@ -32,15 +38,21 @@ namespace hemlock {
             GLuint vbo;
             glGenBuffers(1, &vbo);
             glBindBuffer(GL_ARRAY_BUFFER, vbo);
-            glBufferData(GL_ARRAY_BUFFER, sizeof(f32) * 3 * vertexData.positionCount, vertexData.positions, (int)volatility);
-
+            glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex3D<FXX>) * vertexData.vertexCount, vertexData.vertices, (int)volatility);
+            
             GLuint ebo;
             glGenBuffers(1, &ebo);
             glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(f32) * vertexData.indexCount, vertexData.indices, (int)volatility);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(FXX) * vertexData.indexCount, vertexData.indices, (int)volatility);
 
-            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(f32), 0);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex3D<FXX>), 0);
             glEnableVertexAttribArray(0);
+
+            glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex3D<FXX>), (GLvoid*)(3 * sizeof(FXX)));
+            glEnableVertexAttribArray(1);
+
+            glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex3D<FXX>), (GLvoid*)(7 * sizeof(FXX)));
+            glEnableVertexAttribArray(2);
 
             glBindVertexArray(0);
             glBindBuffer(GL_ARRAY_BUFFER, 0);
