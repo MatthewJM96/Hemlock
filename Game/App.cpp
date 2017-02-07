30cb239731a624a8a151080fcd91c2a444a11b35
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-02-07 20:31:02 +0000

    Remove all the shit.

--- Game/App.cpp
+++ Game/App.cpp
@@ -3,79 +3,33 @@
 #include "App.h"
 
 #include "MainMenuScreen.h"
-
+#include "VoxelTestScreen.h"
 
 void App::init() {
     if (m_initialised) return;
     IApp::init();
-
-    m_window->setDimensions({ 640, 320 });
-
-    const auto& dispatcher = hui::InputDispatcher::getInstance();
-    dispatcher->onMouseButtonDown += h::makeDelegate(*this, &App::handleMouseButtonDown);
-    dispatcher->onMouseMove += h::makeDelegate(*this, &App::handleMouseMovement);
-    dispatcher->onMouseButtonUp += h::makeDelegate(&App::handleMouseButtonUp);
-    dispatcher->onKeyboardButtonUp += h::makeDelegate(*this, &App::handleKeyboardButtonDown);
+    
+    hui::InputDispatcher::getInstance()->onKeyboardButtonUp += { 1, h::makeRDelegate(*this, &App::handleKeyboardButtonDown) };
 }
 
 void App::prepareScreens() {
-    Screen screen;
-    screen.first = "main_menu";
-    screen.second = new MainMenuScreen();
-    addScreen(screen);
+    Screen mainmenu;
+    mainmenu.first = "main_menu";
+    mainmenu.second = new MainMenuScreen();
+    addScreen(mainmenu);
 
-    setCurrentScreen(screen.first);
+    Screen voxeltest;
+    voxeltest.first = "voxel_test";
+    voxeltest.second = new VoxelTestScreen(m_inputManager);
+    addScreen(voxeltest);
+
+    setCurrentScreen(voxeltest.first);
 }
 
-void App::handleMouseButtonDown(h::Sender sender, hui::MouseButtonEvent event) {
-    puts("A button was pressed!\n");
-
-    char* bType;
-    switch (event.button) {
-        case hui::MouseButton::LEFT:
-            bType = "left";
-            break;
-        case hui::MouseButton::RIGHT:
-            bType = "right";
-            break;
-        case hui::MouseButton::MIDDLE:
-            bType = "middle";
-            break;
-        default:
-            bType = "unknown";
-    }
-
-    printf("Button type was %s.\n", bType);
-}
-
-void App::handleMouseMovement(h::Sender sender, hui::MouseMoveEvent event) const {
-    printf("xRel: %d   ---   yRel: %d\n", event.dx, event.dy);
-}
-
-void App::handleMouseButtonUp(h::Sender sender, hui::MouseButtonEvent event) {
-    puts("A button was released!\n");
-
-    char* bType;
-    switch (event.button) {
-        case hui::MouseButton::LEFT:
-            bType = "left";
-            break;
-        case hui::MouseButton::RIGHT:
-            bType = "right";
-            break;
-        case hui::MouseButton::MIDDLE:
-            bType = "middle";
-            break;
-        default:
-            bType = "unknown";
-    }
-
-    printf("Button type was %s.\n", bType);
-}
-
-void App::handleKeyboardButtonDown(h::Sender sender, hui::KeyboardButtonEvent event) {
-    if (event.physicalKey == SDL_SCANCODE_ESCAPE) {
+bool App::handleKeyboardButtonDown(h::Sender sender, hui::KeyboardButtonEvent event) {
+    if (event.physicalKey == hui::PhysicalKey::H_ESCAPE) {
         SDL_Quit();
         exit(0);
     }
+    return true;
 }
--- Game/App.h
+++ Game/App.h
@@ -11,8 +11,5 @@ public:
 protected:
     void prepareScreens();
 private:
-    void handleMouseButtonDown(h::Sender sender, hui::MouseButtonEvent event);
-    void handleKeyboardButtonDown(h::Sender sender, hui::KeyboardButtonEvent event);
-    void handleMouseMovement(h::Sender sender, hui::MouseMoveEvent event) const;
-    static void handleMouseButtonUp(h::Sender sender, hui::MouseButtonEvent event);
+    bool handleKeyboardButtonDown(h::Sender sender, hui::KeyboardButtonEvent event);
 };
