41d357ace9c347c597ed0882212da585e5b7a4b8
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-05-04 12:52:35 +0100

    Improve ChunkID implementation.

--- Hemlock/include/voxel/ChunkCoordSystems.h
+++ Hemlock/include/voxel/ChunkCoordSystems.h
@@ -2,11 +2,11 @@
 
 namespace hemlock {
     namespace voxel {
-        // Names verbose others may be necessary if we choose to support (or if games choose to use anyway) non-planar topologies for chunkable spaces.
+        // Names verbose, but others may be necessary if we choose (or if games choose regardless) to support non-rectilinear topologies for chunkable spaces.
 
         /// Spaces:
         // World-space: any space in which the player interacts directly.
-        //              -> contains a grid-space of chunks, which themselves contain a grid-space of blocks.
+        //              -> contains a grid-space of chunks, which themselves contain a chunk-space of blocks.
         // Grid-space:  discrete space with units of chunk width (i.e. each chunk is at an integer coordinate).
         //              -> used to reference locations of chunks in the world relative to one another.
         // Chunk-space: discrete space with units of block width (i.e. each block is at an integer coordinate).
@@ -28,7 +28,12 @@ namespace hemlock {
         };
         // Chunk position in rectilinear world-space.
         struct ChunkRectilinearWorldPosition {
-            i32 x, y, z;
+            struct {
+                i64 x : 24;
+                i64 y : 16;
+                i64 z : 24;
+            };
+            ui64 id;
         };
         using ChunkID = ChunkRectilinearWorldPosition;
 
@@ -45,12 +50,11 @@ namespace hemlock {
 namespace hvox = hemlock::voxel;
 
 namespace std {
-    // Hash collisions after ~1.6*10^7 chunks in x and z, and after ~6.5*10^4 chunks in y.
     template<>
     struct hash<hvox::ChunkID> {
         std::size_t operator()(const hvox::ChunkID& id) const {
             std::hash<ui64> hash;
-            return hash(id.x + ((ui64)id.y << 24) + ((ui64)id.z << 40));
+            return hash(id.id);
         }
     };
 }
