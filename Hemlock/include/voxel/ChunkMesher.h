de287c16ef4fe44db73c4999ff3d0c595eafe048
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-05-05 04:41:30 +0100

    Remove extraneous things not useful for mesher.

--- Hemlock/include/voxel/ChunkMesher.h
+++ Hemlock/include/voxel/ChunkMesher.h
@@ -4,58 +4,7 @@
 #include "graphics/Mesh.hpp"
 
 namespace hemlock {
-	namespace voxel {
-		// Won't need these once greedy merging is in, but for now let's just 
-		// figure out simple occlusion. As such don't worry about indexing for now.
-		static hg::Vertex3D<f32> FRONT_QUAD[6] = {
-			{ -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },
-			{  0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-			{  0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
-			{  0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
-			{ -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-			{ -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },
-		};
-		static hg::Vertex3D<f32> BACK_QUAD[6] = {
-			{ -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },
-			{  0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-			{  0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
-			{  0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
-			{ -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-			{ -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },
-		};
-		static hg::Vertex3D<f32> LEFT_QUAD[6] = {
-			{ -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-			{ -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
-			{ -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-			{ -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-			{ -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },
-			{ -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-		};
-		static hg::Vertex3D<f32> RIGHT_QUAD[6] = {
-			{  0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-			{  0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
-			{  0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-			{  0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-			{  0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },
-			{  0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-		};
-		static hg::Vertex3D<f32> BOTTOM_QUAD[6] = {
-			{ -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-			{  0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
-			{  0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-			{  0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-			{ -0.5f, -0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },
-			{ -0.5f, -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-		};
-		static hg::Vertex3D<f32> TOP_QUAD[6] = {
-			{ -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f },
-			{  0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f },
-			{  0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-			{  0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f },
-			{ -0.5f,  0.5f,  0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f },
-			{ -0.5f,  0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f }
-		};
-		
+	namespace voxel {		
 		struct ChunkMeshTask {
 			union {
 				ChunkID						  id;
@@ -66,21 +15,7 @@ namespace hemlock {
 
 		class ChunkMesher {
 		public:
-			void init();
-
 			void runMeshTask(ChunkMeshTask task, ui64 size);
-		private:
-			union {
-				GLuint m_quadVAOs[6];
-				struct {
-					GLuint m_frontVAO;
-					GLuint m_backVAO;
-					GLuint m_leftVAO;
-					GLuint m_rightVAO;
-					GLuint m_bottomVAO;
-					GLuint m_topVAO;
-				};
-			};
 		};
 	}
 }
