71e8dbf18c6f7138f2ad7af496465eff39ade745
Matthew Marshall <matthew.marshall96@yahoo.co.uk>
2017-05-06 06:12:44 +0100

    m_chunkPosition -> pos. Untabify file.

--- Hemlock/src/voxel/Chunk.cpp
+++ Hemlock/src/voxel/Chunk.cpp
@@ -2,29 +2,29 @@
 
 #include "voxel/Chunk.h"
 
-void hvox::Chunk::init(ui16 size, ChunkRectilinearWorldPosition chunkPosition) {
-	m_size          = size;
-    m_chunkPosition = chunkPosition;
+void hvox::Chunk::init(ui16 size, ChunkGridPosition chunkPosition) {
+    m_size          = size;
+    pos = chunkPosition;
 
-	blocks = new Block[size * size * size];
+    blocks = new Block[size * size * size];
     std::fill_n(blocks, size * size * size, Block{ false });
 
     neighbours = { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr };
 }
 
 void hvox::Chunk::dispose() {
-	delete[] blocks;
-	blocks = nullptr;
+    delete[] blocks;
+    blocks = nullptr;
 
-	neighbours = { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr };
+    neighbours = { nullptr, nullptr, nullptr, nullptr, nullptr, nullptr };
 }
 
-void hvox::Chunk::setBlock(BlockChunkPosition pos, Block block) {
-	blocks[getBlockIndex(pos, m_size)] = block;
-	onBlockChange({ block.present ? BlockChange::PLACE : BlockChange::DESTROY, pos, m_chunkPosition });
+void hvox::Chunk::setBlock(BlockChunkPosition blockPos, Block block) {
+    blocks[getBlockIndex(blockPos, m_size)] = block;
+    onBlockChange({ block.present ? BlockChange::PLACE : BlockChange::DESTROY, blockPos, pos });
 }
 
 void hvox::Chunk::setContiguousBlocks(BlockChunkPosition start, ui32 count, Block* blocks) {
-	std::memcpy(&blocks[getBlockIndex(start, m_size)], blocks, count);
-	onBulkBlockChange({ blocks, start, count, m_chunkPosition });
+    std::memcpy(&blocks[getBlockIndex(start, m_size)], blocks, count);
+    onBulkBlockChange({ blocks, start, count, pos });
 }
